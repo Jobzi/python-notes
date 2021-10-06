e9786fa0fd9301293dfba511cc7e245b7f1841b0
Jobzi <jipsonmurillo@gmail.com>
2021-10-06 18:02:17 -0500

    desestructuracion

---
+++ 4. desestructuración.py
@@ -0,0 +1,31 @@
+primero,_,tercero,cuarto=['Jipson',"Pepe","Mario","Simon"]
+
+print(tercero)
+
+
+jipsonmurillo = {
+  "pronouns": 'he/him',
+  "code": [
+    {
+      "mobile": {
+        "framework": ['Flutter', 'React Native'],
+        "languages": ['Dart', 'Java', 'JS', 'TS']
+      },
+    }
+  ],
+  "database": ['MySql', 'PostgreSQL', 'MongoDB', 'Firebase'],
+  "tools": [
+    'VS Code ♥',
+    'Node.js',
+    'GraphQL',
+    'Vercel',
+    'RunJs',
+    'Docker',
+    'Jest',
+    'Cypress'
+  ],
+}
+
+[code,database]=jipsonmurillo
+print(code)
+print(database)
