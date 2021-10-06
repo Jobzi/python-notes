3cc9f62695f68dd7822bd0e1c9fff3b94acfe6e1
Jobzi <jipsonmurillo@gmail.com>
2021-10-06 15:47:28 -0500

    add readme

--- 3. dirc.py
+++ 3. dirc.py
@@ -8,3 +8,34 @@ dict.get("key")  # ...this returns None
 dict.setdefault("key", 1)
 **dict           # expands all k/v pairs in place
 """
+
+jipsonmurillo = {
+  "pronouns": 'he/him',
+  "code": [
+    {
+      "mobile": {
+        "framework": ['Flutter', 'React Native'],
+        "languages": ['Dart', 'Java', 'JS', 'TS']
+      },
+    }
+  ],
+  "database": ['MySql', 'PostgreSQL', 'MongoDB', 'Firebase'],
+  "tools": [
+    'VS Code ♥',
+    'Node.js',
+    'GraphQL',
+    'Vercel',
+    'RunJs',
+    'Docker',
+    'Jest',
+    'Cypress'
+  ],
+}
+
+
+print(jipsonmurillo.keys())
+print(jipsonmurillo.values())
+print(jipsonmurillo.get("code"))
+
+for key, val in jipsonmurillo.items():
+    print(key)
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Hello This is a Review about Python and Typing
+
+- type
+```python
+variable:int|str|bool|float ...
+
+def holaMundo( say:str ) -> str:
+    return "Hola "+say
+```
+- Guide [devhints](""https://devhints.io/python") of Python
