1714bc610af038c5126ea29eae2f1a6251d0b853
Doremon <www.saylipatil1603@gmail.com>
2022-10-01 20:08:41 +0530

    usage of random module
    how to choose random value from array

---
+++ 12.random-module.py
@@ -0,0 +1,12 @@
+
+import random
+
+# list with some values
+card_deck = ['Ace','2','3','4','5','6','7','8','9','10','jack','Queen','King']
+
+
+# choice function returns random value
+random_card = random.choice(card_deck)
+
+# print random value
+print("your got this card: ", random_card)
